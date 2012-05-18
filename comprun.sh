317a8e05fa211d9fb8d65653b60eae970b2ab05e
Maksim Stepanenko <maksimus16@gmail.com>
2012-05-18 02:54:17 -0400

    everything ready, but not tested. also need to add array of words for string tests

--- comprun.sh
+++ comprun.sh
@@ -1,12 +1,13 @@
 gcc -o tab_hash tab_hash.c -pg
-./tab_hash > test1.csv
-sleep 1
-./tab_hash > test2.csv
-sleep 1
-./tab_hash > test3.csv
-sleep 1
-./tab_hash > test4.csv
-sleep 1
-./tab_hash > test5.csv
-python combine_data.py
-cat result.csv
+#./tab_hash > test1.csv
+#sleep 1
+#./tab_hash > test2.csv
+#sleep 1
+#./tab_hash > test3.csv
+#sleep 1
+#./tab_hash > test4.csv
+#sleep 1
+#./tab_hash > test5.csv
+#python combine_data.py
+#cat result.csv
+./tab_hash
